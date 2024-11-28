84ab78a9839b05acbc2b58a2a137c5276cf69212
Vasiliy <vassiliyprm@gmail.com>
2024-11-28 16:12:52 +0500

    добавлена функция выгрузки в файл json

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
+import pyperclip
+import json
+import os
 
+history_file = "upload-history.json"
+
+
+def save_history(file_path, link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, "r") as f:
+            history = json.load(f)
+    history.append({file_path: os.path.basename(file_path), "'download_link": link})
+    with open(history_file, "w") as f:
+        json.dump(history, f, indent=4)
 def upload():
     try:
         filepath = fd.askopenfilename()
@@ -15,6 +29,9 @@ def upload():
                 link = response.json()["link"]
                 entry.delete(0, END)
                 entry.insert(0,link)
+                pyperclip.copy(link)
+                save_history(filepath, link)
+                mb.showinfo("Загрузка файла", "Ссылка скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
