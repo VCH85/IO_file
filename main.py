0d3763fe7743393c780e4168a8564ff5edf0e4e4
Vasiliy <vassiliyprm@gmail.com>
2024-11-28 18:06:53 +0500

    Тестирование функции save_history

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import os
 history_file = "upload-history.json"
 
 
-def save_history(file_path, link):
+def save_history(file_path, download_link):
     history = []
     if os.path.exists(history_file):
         with open(history_file, "r") as f:
             history = json.load(f)
-    history.append({file_path: os.path.basename(file_path), "'download_link": link})
+    history.append({"file_path": os.path.basename(file_path), "download_link": download_link})
     with open(history_file, "w") as f:
         json.dump(history, f, indent=4)
 def upload():
@@ -26,11 +26,11 @@ def upload():
                 files = {"file": f}
                 response = requests.post("https://file.io", files = files)
                 response.raise_for_status()
-                link = response.json()["link"]
+                download_link = response.json().get("link")
                 entry.delete(0, END)
-                entry.insert(0,link)
-                pyperclip.copy(link)
-                save_history(filepath, link)
+                entry.insert(0,download_link)
+                pyperclip.copy(download_link)
+                save_history(filepath, download_link)
                 mb.showinfo("Загрузка файла", "Ссылка скопирована в буфер обмена")
     except Exception as e:
         mb.showerror("Ошибка", f"Произошла ошибка: {e}")
@@ -43,10 +43,10 @@ def show_history():
 
     history_window = Toplevel(window)
     history_window.title("История загрузок")
-    file_listbox = Listbox(history_window, width = 50, height=20)
-    file_listbox.grid(row=0, column = 0, padx=(10,0), pady=10)
+    file_listbox = Listbox(history_window, width=50, height=20)
+    file_listbox.grid(row=0, column=0, padx=(10,0), pady=10)
     links_listbox = Listbox(history_window, width = 50, height=20)
-    links_listbox.grid(row=0, column = 1, padx=(0,10), pady=10)
+    links_listbox.grid(row=0, column=1, padx=(0,10), pady=10)
 
     with open(history_file, 'r') as f:
         history = json.load(f)
@@ -67,9 +67,6 @@ entry.pack()
 
 button_history = ttk.Button(text="История запросов", command= show_history)
 button_history.pack()
-
-
-
 
 window.mainloop()
 
